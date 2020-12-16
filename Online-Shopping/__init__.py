a460343cd133cba08c4862f293b1d6bc93e8ca0e
saeedhmt <58300378+saeedhmt@users.noreply.github.com>
2020-12-16 23:51:09 +0330

    init file modified

---
+++ Online-Shopping/__init__.py
@@ -0,0 +1,30 @@
+from flask import *
+import os
+import json
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.update(test_config)
+
+
+    # app.logger.debug('app.config = %s', app.config)
+
+    # try:
+    #     os.major(app.instance_path)
+    # except OSError:
+    #     pass
+    
+    @app.route('/')
+    def main():
+        return 'Main Page'
+    
+    from . import admin
+    app.register_blueprint(admin.bp)
+    
+    app.add_url_rule('/', endpoint='index')
+    return app
