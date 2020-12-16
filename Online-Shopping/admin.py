1168dd3b285edbc2ccadaa03bbe096b4264b1ef6
saeedhmt <58300378+saeedhmt@users.noreply.github.com>
2020-12-16 23:52:57 +0330

    files for admin panel added.

---
+++ Online-Shopping/admin.py
@@ -0,0 +1,75 @@
+from flask import *
+import functools
+from werkzeug.security import *
+import json
+from cryptography.fernet import Fernet
+
+
+bp = Blueprint('admin', __name__, url_prefix='/admin')
+with open ('admins.json') as f:
+    main = json.load(f)
+
+with open('key.txt') as f:
+    key = f.read()
+
+cipher_suite = Fernet(key.encode())
+admins = {}
+for admin in main[1:]:
+    username = admin[0]
+    password = admin[1]
+    admins[username] = cipher_suite.decrypt(password.encode()).decode()
+
+def valid_admin(username, password):
+    if username in admins.keys():
+        return password == admins[username]
+    else:
+        return 'invalid username'
+
+
+@bp.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if valid_admin(username, password):
+            session.clear()
+            session['admin'] = request.form['username']
+            return redirect(url_for('admin.panel'))
+
+        elif valid_admin(username, password) == 'invalid username':
+             error = 'Username not registered! :('
+
+        else:
+            error = 'Invalid Password'
+
+        flash(error)
+
+    return render_template('admin/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.pop('admin', None)
+    return redirect(url_for('admin.login'))
+
+
+@bp.route('/panel')
+def panel():
+    return render_template('panel.html')
+
+@bp.route('/commodity')
+def commodity():
+    pass
+
+@bp.route('/inventory')
+def inventory():
+    pass
+
+@bp.route('/price')
+def price():
+    pass
+
+@bp.route('/orders')
+def orders():
+    pass
