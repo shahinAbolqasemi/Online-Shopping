c7d55c7f158281ca11487ae9c8b102f266428632
Sh_PSR <shahin1974_27@yahoo.com>
2020-12-30 11:00:48 +0330

    register api.bp, db.init_app for remove g.db

--- online_shopping/__init__.py
+++ online_shopping/__init__.py
@@ -11,7 +11,6 @@ def create_app(test_config=None):
     else:
         app.config.update(test_config)
 
-
     # app.logger.debug('app.config = %s', app.config)
 
     # try:
@@ -19,13 +18,17 @@ def create_app(test_config=None):
     # except OSError:
     #     pass
 
-
     @app.route('/')
     def main():
         return 'Main Page'
-    
+
+    from online_shopping import db
+    db.init_app(app)
+
     from online_shopping import admin
+    from online_shopping import api
     app.register_blueprint(admin.bp)
-    
+    app.register_blueprint(api.bp)
+
     app.add_url_rule('/', endpoint='index')
     return app
