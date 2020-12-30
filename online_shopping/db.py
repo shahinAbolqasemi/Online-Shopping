b83966cd3eec5580997048f5cbdf94353e2d10b2
Sh_PSR <shahin1974_27@yahoo.com>
2020-12-30 11:07:01 +0330

    resolve close_db error, add init_app for teardown_appcontext

--- online_shopping/db.py
+++ online_shopping/db.py
@@ -2,13 +2,18 @@ import pymongo
 from flask import g, current_app
 
 
-# db = client.online_shopping
-
 def get_db():
     if 'db' not in g:
         g.db = pymongo.MongoClient(current_app.config["CONNECTION_STRING_MDB"]).online_shopping
     return g.db
 
 
-def close_db():
+def close_db(e=None):
     db = g.pop('db', None)
+
+
+def init_app(app):
+    """Register database functions with the Flask app. This is called by
+    the application factory.
+    """
+    app.teardown_appcontext(close_db)
