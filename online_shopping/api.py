edb5e02539013994451dae3bc5165c6d013e71f5
Sh_PSR <shahin1974_27@yahoo.com>
2021-01-13 21:09:37 +0330

    complete 90% of APIs

--- online_shopping/api.py
+++ online_shopping/api.py
@@ -20,14 +20,15 @@ def load_():
 @login_required
 def prod_list():
     products = list(get_db('products').find())
-    return jsonify(products)
+    return jsonify(data=products)
 
 
 @bp.route('/product/<product_id>/')
 @login_required
 def prod_details(product_id):
+    print('hello')
     product = get_db('products').find_one({'_id': ObjectId(product_id)})
-    return jsonify(product)
+    return jsonify(data=product)
 
 
 @bp.route('/product/add/', methods=['POST'])
@@ -51,69 +52,177 @@ def prod_add():
     try:
         get_db('products').insert_one(product_document)
     except (Exception) as ex:
-        return jsonify({'status': False})
+        return jsonify(status={'status': False})
     else:
-        return jsonify({'status': True})
+        return jsonify(status={'status': True})
 
 
 @bp.route('/product/edit/', methods=['POST'])
 @login_required
 def prod_edit():
-    return ...
+    products = get_db("products")
+    product_id = request.form.get('productId')
+    # if product_id := request.form.get('productId'):
+    #     product = products.find_one({"_id": ObjectId(product_id)}, {"_id": 0})
+    #     return jsonify(data=product)
+    # else:
+    product_document = {
+        'name': request.form.get('prod-name'),
+        'description': request.form.get('prod-desc'),
+        'category': request.form.get('prod-category')
+    }
+    image_file = request.files.get('prod-image')
+    if image_file:
+        filename = secure_filename(image_file.filename)
+        image_file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        image_url = url_for('uploads', filename=filename)
+        product_document['image'] = image_url
+    try:
+        products = get_db('products').update({ObjectId(product_id)}, {"$set": product_document})
+    except (Exception) as ex:
+        return jsonify(status={'status': False})
+    else:
+        return jsonify(status={'status': True})
 
 
-@bp.route('/product/delete/<int:product_id>/')
+@bp.route('/product/delete/<product_id>/')
 @login_required
 def prod_delete(product_id):
-    return ...
+    try:
+        get_db("products").delete_one({"_id": ObjectId(product_id)})
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/product/add/', methods=['POST'])
 @login_required
 def prod_upload():
+    products_file = request.files.get('products-csv')
     return ...
 
 
 @bp.route('/warehouse/list/')
 @login_required
 def ware_list():
-    return ...
+    warehouses = list(get_db('warehouses').find())
+    return jsonify(data=warehouses)
 
 
 @bp.route('/warehouse/add/', methods=['POST'])
 @login_required
 def ware_add():
-    return ...
+    warehouse_name = request.form.get('warehouse-name')
+    try:
+        get_db("warehouses").insert_one({'name': warehouse_name})
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/warehouse/edit/', methods=['POST'])
 @login_required
 def ware_edit():
-    return ...
+    warehouses = get_db('warehouses')
+    warehouse_id = request.form.get('warehouseId')
+    warehouse_name = request.form.get('warehouse-name')
+    # if warehouse_id := request.form.get('warehouseId'):
+    #     warehouse = warehouses.find({"_id": ObjectId(warehouse_id)})
+    #     return jsonify(data=warehouse)
+    # else:
+    try:
+        warehouses.update_one({"_id": ObjectId(warehouse_id)}, {"$set": {"name": warehouse_name}})
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
-@bp.route('/warehouse/delete/<int:warehouse_id>/')
+@bp.route('/warehouse/delete/<warehouse_id>/')
 @login_required
 def ware_delete(warehouse_id):
-    return ...
+    try:
+        get_db('warehouses').delete_one({"_id": ObjectId(warehouse_id)})
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/quantity/list/')
 @login_required
 def quantity_list():
-    return ...
+    products = get_db("products")
+    quantities = products.aggregate([
+        {
+            '$unwind': {
+                'path': '$warehouses',
+                'preserveNullAndEmptyArrays': False
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'name': 1,
+                'warehouses': 1
+            }
+        }
+    ])
+    return jsonify(data=quantities)
 
 
 @bp.route('/quantity/add/', methods=['POST'])
 @login_required
 def quantity_add():
-    return ...
+    products = get_db('products')
+    product_id = request.form.get('productId')
+    warehouse_name = request.form.get('warehouse-name')
+    warehouse_id = request.form.get('warehouse-id')
+    product_price = request.form.get('product-price')
+    product_quantity = request.form.get('product-quantity')
+    try:
+        products.update_one({
+            "_id": ObjectId(product_id)},
+            {
+                "$push": {
+                    'warehouses': {'_id': ObjectId(warehouse_id), 'name': warehouse_name, 'price': product_price,
+                                   'quantity': product_quantity}
+                }
+            })
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/quantity/edit/', methods=['POST'])
 @login_required
 def quantity_edit():
-    return ...
+    products = get_db('products')
+    product_id = request.form.get('productId')
+    warehouse_id = request.form.get('warehouseId')
+    product_name = request.form.get('product-name')
+    warehouse_name = request.form.get('warehouse-name')
+    product_price = request.form.get('product-price')
+    product_quantity = request.form.get('product-quantity')
+    try:
+        products.update_one(
+            {"_id": ObjectId(product_id), 'warehouses': {"$elemMatch": {"_id": ObjectId(warehouse_id)}}},
+            {"$set": {
+                "name": product_name,
+                'warehouses.$': {
+                    '_id': ObjectId(warehouse_id),
+                    'name': warehouse_name,
+                    'price': product_price,
+                    'quantity': product_quantity
+                }
+            }}
+        )
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/quantity/delete/')
@@ -121,7 +230,13 @@ def quantity_edit():
 def quantity_delete():
     product_id = request.args.get('product')
     warehouse_id = request.args.get('warehouse')
-    return ...
+    products = get_db("products")
+    try:
+        products.update_one({"_id": ObjectId(product_id)}, {"$pull": {"warehouses": {"_id": ObjectId(warehouse_id)}}})
+    except (Exception) as ex:
+        return jsonify(status={'success': False})
+    else:
+        return jsonify(status={'success': True})
 
 
 @bp.route('/order/list/')
